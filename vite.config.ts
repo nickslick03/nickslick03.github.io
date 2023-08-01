e95f87e88e570a117afdb105af9664443ca4a815
nickslick03 <nicholasepps03@gmail.com>
2023-08-01 01:18:57 -0400

    removed base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import solidPlugin from 'vite-plugin-solid';
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-  base: '/',
   plugins: [
     /* 
     Uncomment the following line to enable solid-devtools.
